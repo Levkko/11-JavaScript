225afdc914e47cd8b653a2301d43bda2a1b3eb86
Levko <ganchuklevko@gmail.com>
2025-04-08 11:27:11 +0300

    Update 0.1. Фільтрувати унікальні елементи масиву.js

--- 0.1. Фільтрувати унікальні елементи масиву.js
+++ 0.1. Фільтрувати унікальні елементи масиву.js
@@ -7,3 +7,16 @@ let values = ["Hare", "Krishna", "Hare", "Krishna",
 ];
 
 alert( unique(values) ); // Hare, Krishna, :-O
+
+
+
+//Нове рішення з Set
+function unique(arr) {
+  return Array.from(new Set(arr));
+}
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert(unique(values)); // Hare, Krishna, :-O
